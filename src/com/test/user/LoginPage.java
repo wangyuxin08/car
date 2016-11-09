3984324cb3a6488ec07abe9dfc14bba32fadb2f6
admin <admin@Lenovo-PC>
2016-11-09 09:15:33 +0800

    yes

--- src/com/test/user/LoginPage.java
+++ src/com/test/user/LoginPage.java
@@ -1,13 +1,42 @@
 package com.test.user;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	    WebDriver driver = null;
+	   WebDriver driver = null;
+	   
+	    @FindBy(linkText="µÇÂ¼")
+	    public WebElement login;
+	    
+	    @FindBy(id="name")
+	    public WebElement userName;
+	    
+	    @FindBy(id="password")
+	    public WebElement passWord;
+	    
+	    @FindBy(id="loginbtn")
+	    public WebElement loginbtn;
+	    
 	    public LoginPage(){
 	    	driver = new FirefoxDriver();
 			PageFactory.initElements(driver, this);
+	    }
+	    
+	    public void load(String Url){
+	    	  driver.get(Url);
+	    }
+	    
+	    public void Alert(){
+	    	Alert alert = this.driver.switchTo().alert();
+	    	alert.accept();
+	    }
+	        
+	    public WebDriver getDriver(){
+	    	  return this.driver;
 	    }
 }
--- src/com/test/user/login.java
+++ src/com/test/user/login.java
@@ -1,5 +1,17 @@
 package com.test.user;
 
+import org.testng.annotations.Test;
+
 public class login {
-      
-}
+         String Url="http://localhost:8032/truckhome/";
+         @Test
+         public void login(){
+        	 LoginPage loginpage =new LoginPage();
+        	 loginpage.load(this.Url);
+        	 loginpage.login.click();
+        	 loginpage.userName.sendKeys("admin");
+        	 loginpage.passWord.sendKeys("admin123");
+        	 loginpage.loginbtn.click();
+        	 loginpage.Alert();
+         }
+} 
